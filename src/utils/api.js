85d7bc150c0416c9b013ab71390583367a6b985f
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-08-06 18:22:21 -0300

    att

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://meupetproject.herokuapp.com/",
-});
-// export default axios.create({
-//   baseURL: "http://localhost:5000",
-// });
+ export default axios.create({
+   baseURL: "http://localhost:3000",
+ });
